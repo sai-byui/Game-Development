d4e7ee25211db8eaf7939fa3d38c951a84ac3249
axle12693 <axle12693@gmail.com>
2018-03-07 16:18:56 -0700

    Created Actor, Player, Enemy classes
    Player & Enemy inherit from Actor.
    
    Both Player and Enemey can attack each other, instead of being
    one-sided.
    
    Enemy has "rage" and a special attack.

--- FuzzyLogic.py
+++ FuzzyLogic.py
@@ -1,84 +1,124 @@
-import os
+# import os
 import time
-import random
+# import random
 
 
-class Player:
+class Actor:
     def __init__(self):
-        self.hitPoints = 100
-        self.mana = 100
-
-        # Fuzzy set
+        self.hit_points = 100
+        self.magic_load = 1.0
         self.healthy = 1.0
-        self.magicLoad = 1.0
+        self.mana = 100
         self.coverage = 0.0
+        self.name = ""
 
-    def setHealthy(self, hitPoints):
-        self.healthy = hitPoints * .01
+    def set_healthy(self, hit_points):
+        self.healthy = hit_points * .01
 
-    def setMagicLoad(self, mana):
-        self.magicLoad = mana * .01
+    def set_magic_load(self, mana):
+        self.magic_load = mana * .01
 
-    def magicAttack(self, enemy):
-        enemy.hitPoints -= 10
+    def take_damage(self, amt):
+        self.hit_points -= amt
+        self.set_healthy(self.hit_points)
+        print(self.name + ", You've been hit dude!")
+
+
+class Player(Actor):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.name = "player"
+        pass
+
+    def magic_attack(self, target):
+        print(self.name + " is attacking with magic!")
+        target.take_damage(10)
         self.mana -= 20
-        self.setMagicLoad(self.mana)
-        print("attacking with magic!")
+        self.set_magic_load(self.mana)
 
-    def physicalAttack(self, enemy):
-        enemy.hitPoints -= 15
-        print("Slapping with hand!")
+    def physical_attack(self, target):
+        print(self.name + " is Slapping with hand!")
+        target.take_damage(15)
 
     def hide(self):
-        print("Running and Hiding")
+        print(self.name + " is Running and Hiding")
         self.coverage = 1
 
-    def drinkPotion(self):
-        self.hitPoints += 30
-        self.setHealthy(self.hitPoints)
-        print("Drinking health juice!")
+    def drink_potion(self):
+        self.hit_points += 30
+        self.set_healthy(self.hit_points)
+        print(self.name + " is Drinking health juice!")
 
-    def returnToFight(self):
+    def return_to_fight(self):
         self.coverage = 0.0
-        print("Coming back out to fight!")
+        print(self.name + " is Coming back out to fight!")
 
-    def takeDamage(self, character):
-        self.hitPoints -= 25
-        self.setHealthy(self.hitPoints)
-        print("\nYou've been hit dude!")
 
-hero = Player()
-enemy = Player()
+class Enemy(Actor):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.rage = 0.0  # fuzzy
+        self.name = "enemy"
 
-# This loop implements different actions based on the fuzzy logic
-while enemy.hitPoints > 0:
-    # take action every three seconds
-    time.sleep(3)
-    # if the hero has good health and magic, do a magic attack
-    if hero.healthy > .5 and hero.magicLoad > 0:
-        hero.magicAttack(enemy)
-    # once we run out of magic, do physicalAttack
-    elif hero.healthy > 0.5:
-        hero.physicalAttack(enemy)
-    # if we take too much damage, hide
-    elif hero.coverage < 1:
-        hero.hide()
-        # once we have hidden, take potions to heal
+    def special_attack(self, target):
+        print(self.name + " is using special attack!")
+        target.take_damage(30)
+        self.rage = 0.0
+
+    def magic_attack(self, target):
+        print(self.name + " is attacking with magic!")
+        target.take_damage(10)
+        self.mana -= 20
+        self.set_magic_load(self.mana)
+        self.rage += 0.3
+
+    def physical_attack(self, target):
+        target.take_damage(15)
+        self.rage += 0.5
+        print(self.name + " is Slapping with hand!")
+
+
+def main():
+    hero = Player()
+    enemy = Enemy()
+
+    # This loop implements different actions based on the fuzzy logic
+    while enemy.hit_points > 0 and hero.hit_points > 0:
+        # take action every three seconds
+        time.sleep(3)
+        # if the hero has good health and magic, do a magic attack
+        if hero.healthy > .5 and hero.magic_load > 0:
+            hero.magic_attack(enemy)
+        # once we run out of magic, do physicalAttack
+        elif hero.healthy > 0.5:
+            hero.physical_attack(enemy)
+        # if we take too much damage, hide
+        elif hero.coverage < 1:
+            hero.hide()
+            # once we have hidden, take potions to heal
+        else:
+            hero.drink_potion()
+            time.sleep(2)
+            hero.return_to_fight()
+
+        if hero.coverage == 0.0:
+            if enemy.rage >= 1.0:
+                enemy.special_attack(hero)
+            elif enemy.magic_load > 0.0:
+                enemy.magic_attack(hero)
+            else:
+                enemy.physical_attack(hero)
+        else:
+            print("Hero is hidden!")
+
+        print("Player HP: " + str(hero.hit_points))
+        print("Enemy HP: " + str(enemy.hit_points))
+        print()
+
+    if enemy.hit_points <= 0:
+        print("The enemy is dead!")
     else:
-        hero.drinkPotion()
-        time.sleep(2)
-        hero.returnToFight()
+        print("You have died!")
+    exit()
 
-    hitGenerator = random.random()
-    if hitGenerator > .6 and hero.coverage < 1:
-        hero.takeDamage(hero)
-
-
-print("The enemy is dead!")
-exit()
-
-
-
-
-
-
+main()
